a91a31e0a445609f935cb211c691cdf5bf12c587
Courtney Kamansky <34107543+cn20kaman@users.noreply.github.com>
2020-04-26 11:59:53 -0400

    lint css task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,19 @@ const { src, watch, dest, series } = require(`gulp`);
 const browserSync = require(`browser-sync`);
 const reload = browserSync.reload;
 const htmlValidator = require(`gulp-html`);
+const cssLinter = require(`gulp-stylelint`);
 let validateHTML = () => {
     return src(`html/*.html`)
         .pipe(htmlValidator());
+};
+let lintCSS = () => {
+    return src(`css/*.css`)
+        .pipe(cssLinter({
+            failAfterError: true,
+            reporters: [
+                {formatter: `verbose`, console: true}
+            ]
+        }));
 };
 let serve = () => {
     browserSync({
@@ -21,4 +31,5 @@ let serve = () => {
     watch([`html/**/*.html`,`css/*.css`,`js/*.js`]).on(`change`, reload);
 };
 exports.validateHTML = validateHTML;
+exports.lintCSS = lintCSS;
 exports.default = serve;
