71e752830c0450558cbb26f059a8c179daa1e273
Courtney Kamansky <34107543+cn20kaman@users.noreply.github.com>
2020-04-26 12:21:07 -0400

    function to update the recipe measurements and title

--- js/app.js
+++ js/app.js
@@ -15,4 +15,21 @@ window.onload = () => {
         updateRecipe(e.target.value);
         ounces.value = 1;
     };
+
+    let updateRecipe = function(riceType, numOfOunces = 1) {
+        if(numOfOunces===``){numOfOunces=1;}
+        let whiteRice = `For slightly al dente: </br> Combine ` + numOfOunces +
+        ` ounces of rice with ` + numOfOunces * 1.6 +
+        ` ounces of water or broth and 1 Tbsp olive oil. Bring to a boil and stir `+
+        ` once to mix. Reduce heat to low, cover with a tight-fitting lid and `+
+        ` cook for 25 minutes. Remove from heat and let stand for 5 minutes. ` +
+        `Fluff with a fork and serve. </br> </br> For softer rice: </br> Increase `+
+        `liquid by ` + numOfOunces * 0.4 + ` and cook time by 5 minutes.`;
+        let caliRice = `Combine ` + numOfOunces + ` ounces of rice with ` +
+        numOfOunces * 2 + ` ounces of water and 1 Tbsp olive oil. Bring to a boil, `
+        + `then reduce heat to the lowest setting. Cook for about 18 minutes.`;
+        if(riceType===`White Rice`){recipe.innerHTML=whiteRice;}
+        else{recipe.innerHTML=caliRice;}
+        title.innerHTML=riceType;
+    };
 };
