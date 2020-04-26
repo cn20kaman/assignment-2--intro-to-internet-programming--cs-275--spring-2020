aa652f15c1e30f1f6552e2b8b76156877ea8e89a
Courtney Kamansky <34107543+cn20kaman@users.noreply.github.com>
2020-04-26 12:14:42 -0400

    attach html elements

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 window.onload = () => {
+    let recipe = document.getElementById(`recipe`);
+    let ounces = document.getElementById(`ounces`);
+    let title = document.getElementById(`title`);
+    let riceSelector = document.getElementById(`type-select`);
+    let riceType = riceSelector.value;
 };
