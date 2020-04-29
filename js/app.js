ea337d0de45cb4fc63c6008ff755a40c3ed86654
Courtney Kamansky <34107543+cn20kaman@users.noreply.github.com>
2020-04-29 16:42:42 -0400

    adjust rounding for ounces

--- js/app.js
+++ js/app.js
@@ -18,15 +18,18 @@ window.onload = () => {
 
     let updateRecipe = function(riceType, numOfOunces = 1) {
         if(numOfOunces===``){numOfOunces=1;}
-        let whiteRice = `For slightly al dente: </br> Combine ` + numOfOunces +
-        ` ounces of rice with ` + numOfOunces * 1.6 +
-        ` ounces of water or broth and 1 Tbsp olive oil. Bring to a boil and stir `+
+        let caliRice = `For slightly al dente: </br> Combine ` + numOfOunces +
+        ` ounces of rice with ` + (numOfOunces * 1.6).toFixed(2) +
+        ` ounces of water or broth and ` + (numOfOunces * 0.0625).toFixed(2) +
+        ` ounces of olive oil. Bring to a boil and stir `+
         ` once to mix. Reduce heat to low, cover with a tight-fitting lid and `+
         ` cook for 25 minutes. Remove from heat and let stand for 5 minutes. ` +
         `Fluff with a fork and serve. </br> </br> For softer rice: </br> Increase `+
-        `liquid by ` + numOfOunces * 0.4 + ` and cook time by 5 minutes.`;
-        let caliRice = `Combine ` + numOfOunces + ` ounces of rice with ` +
-        numOfOunces * 2 + ` ounces of water and 1 Tbsp olive oil. Bring to a boil, `
+        `liquid by ` + (numOfOunces * 0.4).toFixed(2) + ` ounces and cook time`+
+        ` by 5 minutes.`;
+        let whiteRice = `Combine ` + numOfOunces + ` ounces of rice with ` +
+        (numOfOunces * 2).toFixed(2) + ` ounces of water and ` +
+        (numOfOunces * 0.0625).toFixed(2) + ` ounces of olive oil. Bring to a boil, `
         + `then reduce heat to the lowest setting. Cook for about 18 minutes.`;
         if(riceType===`White Rice`){recipe.innerHTML=whiteRice;}
         else{recipe.innerHTML=caliRice;}
